8d97f6572a5a27aee530ca8bccfb917c22ec43d5
Scott Schwartz <scottschwartz97@gmail.com>
2025-04-07 12:57:34 -0400

    basic board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import "./App.css";
 
+function Square({ letter }) {
+  return <div className="square">{letter}</div>;
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
+      <div className="board-row">
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+        <Square letter="p" />
+      </div>
     </div>
   );
 }
