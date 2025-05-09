d898d59ae610fee0ff84166a4db3e72c5c76fa33
Scott Schwartz <scottschwartz97@gmail.com>
2025-05-09 17:41:29 -0400

    working updated probability

--- src/AISolver.ts
+++ src/AISolver.ts
@@ -1,10 +1,12 @@
 import { Yallist } from "yallist";
 import allWords from "./data/wordle-All.json";
 import lettersProbabilities from "./data/letterProbabilities.json";
+import wordProbabilities from "./data/wordProbabilities.json";
 
 export class AISolver {
   allWordsList: Yallist<string>;
   lettersProbabilities: Record<string, number>;
+  wordProbabilities: Record<string, number>;
   blackLetters: Set<string>;
   yellowLetters: Map<number, Set<string>>;
   greenLetters: Map<number, string | null>;
@@ -12,6 +14,7 @@ export class AISolver {
   constructor() {
     this.allWordsList = Yallist.create(allWords);
     this.lettersProbabilities = lettersProbabilities;
+    this.wordProbabilities = wordProbabilities;
     this.blackLetters = new Set<string>();
     this.yellowLetters = new Map<number, Set<string>>();
     this.greenLetters = new Map<number, string | null>();
@@ -43,8 +46,9 @@ export class AISolver {
     let currWordNode = this.allWordsList.head;
     while (currWordNode) {
       const nextNode = currWordNode.next;
-      let currProb = 0;
       const lettersProcessed: Set<String> = new Set();
+      let doubleLetters = false;
+      let hasBeenPruned = false;
 
       for (let i = 0; i < currWordNode.value.length; i++) {
         const currGuessedLetter: string = currWordNode.value[i];
@@ -56,18 +60,23 @@ export class AISolver {
             this.greenLetters.get(i) !== currGuessedLetter)
         ) {
           this.allWordsList.removeNode(currWordNode);
+          hasBeenPruned = true;
           break;
         } else {
           if (lettersProcessed.has(currGuessedLetter)) {
-            currProb += this.lettersProbabilities[currGuessedLetter] / 2;
-          } else {
-            currProb += this.lettersProbabilities[currGuessedLetter];
+            doubleLetters = true;
           }
           lettersProcessed.add(currGuessedLetter);
         }
       }
 
-      if (currProb >= currBestProb) {
+      let currProb = this.wordProbabilities[currWordNode.value];
+
+      if (doubleLetters) {
+        currProb *= 0.05;
+      }
+
+      if (!hasBeenPruned && currProb >= currBestProb) {
         currBestProb = currProb;
         currBestGuess = currWordNode.value;
       }
