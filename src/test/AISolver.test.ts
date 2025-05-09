383b5ace86b3fa26928c6afd990926588f33fb16
Scott Schwartz <scottschwartz97@gmail.com>
2025-05-09 11:44:40 -0400

    tests created for AISolver

--- src/test/AISolver.test.ts
+++ src/test/AISolver.test.ts
@@ -1,6 +1,3 @@
-import { Yallist } from "yallist";
-import allWords from "./data/wordle-All.json";
-import lettersProbabilities from "./data/letterProbabilities.json";
 import { AISolver } from "../AISolver";
 
 let aiSolver: AISolver;
@@ -53,5 +50,31 @@ describe("update", () => {
     expect(aiSolver.greenLetters).toEqual(testMap);
     expect(aiSolver.greenLetters.get(1)).toEqual(null);
     expect(aiSolver.greenLetters.get(2)).toEqual("two");
+  });
+});
+
+describe("solve", () => {
+  beforeEach(() => {
+    aiSolver.update(
+      new Set(["a", "b"]),
+      new Map<number, Set<string>>([
+        [0, new Set("z")],
+        [1, new Set()],
+        [2, new Set()],
+        [3, new Set()],
+        [4, new Set()],
+      ]),
+      new Map<number, string | null>([
+        [0, "z"],
+        [1, null],
+        [2, null],
+        [3, "i"],
+        [4, null],
+      ])
+    );
+  });
+
+  it("solve returns zymic", () => {
+    expect(aiSolver.solve()).toBe("zymic");
   });
 });
